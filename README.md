ffd3efe23866087357f4ef341997ca647c0a2b34
Haritha Geemal <128626728+HarithaGeemal@users.noreply.github.com>
2025-10-27 00:09:58 +0530

    ops manager cleared

--- README.md
+++
@@ -1,1 +0,0 @@
-# Web-Based-Bus-Scheduling-and-Booking-System
--- SeatBookingSystem/src/main/java/com/busbooking/seatbooking/exception/BookingNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.busbooking.seatbooking.exception;
-
-public class BookingNotFoundException extends RuntimeException {
-    public BookingNotFoundException(String message) {
-        super(message);
-    }
-}
-
